b9a138dec9c81d3b94511a52d2aceff32fe2c424
20043 <fahmi.muhazir.12@gmail.com>
2022-10-10 17:47:18 +0700

    Aplikasi Frontend

---
+++ Aplikasi/src/pages/products.js
@@ -0,0 +1,22 @@
+// global imports
+import '../toggleSidebar.js';
+import '../cart/toggleCart.js';
+import '../cart/setupCart.js';
+
+//  filter imports
+import setupSearch from '../filter/search.js';
+import setupCompanies from '../filter/companies.js';
+import setupPrice from '../filter/price.js'
+// specific imports
+import { store } from '../store.js';
+import display from '../displayProducts.js';
+import { getElement } from '../utils.js';
+
+const loading = getElement('.page-loading');
+
+display(store, getElement('.products-container'));
+
+setupSearch(store);
+setupCompanies(store);
+setupPrice(store);
+loading.style.display = 'none';
